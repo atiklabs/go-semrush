32559550026bf454c2f1835c84cb273720e7a21b
Atik <atiklabs@pm.me>
2016-06-22 02:32:16 +0300

    Conection to SEMRush API ok.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"encoding/csv"
 	"strings"
 	"io"
+	"net/url"
 )
 
 type result struct {
@@ -17,18 +18,19 @@ type result struct {
 func main() {
 	file := flag.String("f", "", "CSV file with the url in the first column")
 	apiKey := flag.String("api", "", "API Key given by SEMRush")
+	lang := flag.String("lang", "us", "SEMRush language database")
 	flag.Parse()
 	if *file == "" || *apiKey == "" {
 		flag.Usage()
 	} else {
 		urlList := parseFile(file)
-		processUrlList(urlList, apiKey)
+		processUrlList(urlList, apiKey, lang)
 	}
 }
 
 func parseFile(file *string) []string {
 	dat, err := ioutil.ReadFile(*file)
-	check(err)
+	CheckError(err)
 
 	r := csv.NewReader(strings.NewReader(string(dat)))
 	var urlList []string
@@ -37,20 +39,27 @@ func parseFile(file *string) []string {
 		if err == io.EOF {
 			break
 		}
-		check(err)
+		CheckError(err)
 		urlList = append(urlList, record[0])
 	}
 	return urlList
 }
 
-func processUrlList(urlList []string, apiKey *string) {
+func processUrlList(urlList []string, apiKey *string, lang *string) {
 	for i := 0; i < len(urlList); i++ {
-		fmt.Printf("URL: %s\n", urlList[i])
-		LetsPrint()
+		domain := getDomainFromUrl(urlList[i])
+		score := GetDomainScore(domain, apiKey, lang)
+		fmt.Printf("%d\t%s\t%s\t%d\n", i, urlList[i], domain, score)
 	}
 }
 
-func check(e error) {
+func getDomainFromUrl(address string) string {
+	u, err := url.Parse(address)
+	CheckError(err)
+	return u.Host
+}
+
+func CheckError(e error) {
 	if e != nil {
 		panic(e)
 	}
--- semrush-api.go
+++ semrush-api.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
 
 const (
 	SEMRUSH_API = "http://api.semrush.com/"
 )
 
-func LetsPrint() {
-	fmt.Println(SEMRUSH_API + "hi")
+func GetDomainScore(domain string, apiKey *string, lang *string) int {
+	address := SEMRUSH_API + "?key=" + *apiKey + "&database=" + *lang + "&type=domain_ranks&export_columns=Dn,Ot" + "&domain=" + domain
+	res, err := http.Get(address)
+	CheckError(err)
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	CheckError(err)
+	fmt.Println(address)
+	fmt.Printf("%s", body)
+	return 1
 }
