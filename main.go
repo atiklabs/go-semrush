a324eab542be955ec9e6ccd873a0b488dc5e08ba
Atik <atik.mail@protonmail.com>
2016-06-22 11:14:08 +0300

    Now the program appends a field with the domain traffic to the CSV.

--- main.go
+++ main.go
@@ -10,48 +10,59 @@ import (
 	"net/url"
 )
 
+type csvEntry struct {
+	url string
+	csv string
+}
+
 var scoreMap = map[string]int{}
 
 func main() {
 	file := flag.String("f", "", "CSV file with the url in the first column")
+	header := flag.Bool("h", true, "First row of file is header")
 	apiKey := flag.String("api", "", "API Key given by SEMRush")
 	lang := flag.String("lang", "us", "SEMRush language database")
 	flag.Parse()
 	if *file == "" || *apiKey == "" {
 		flag.Usage()
 	} else {
-		urlList := parseFile(file)
-		processUrlList(urlList, apiKey, lang)
+		csvEntryList := parseFile(file, *header)
+		processUrlList(csvEntryList, apiKey, lang)
 	}
 }
 
 // This parses a CSV file and returns an array with every url
-func parseFile(file *string) []string {
+func parseFile(file *string, header bool) []csvEntry {
 	dat, err := ioutil.ReadFile(*file)
 	CheckError(err)
 	r := csv.NewReader(strings.NewReader(string(dat)))
-	var urlList []string
+	var csvEntryList []csvEntry
 	for {
 		record, err := r.Read()
+		if header {
+			fmt.Println(strings.Join(record, ",") + ",Traffic By Semrush")
+			header = false
+			continue
+		}
 		if err == io.EOF {
 			break
 		}
 		CheckError(err)
-		urlList = append(urlList, record[0])
+		csvEntryList = append(csvEntryList, csvEntry{record[0], "\"" + strings.Join(record, "\",\"") + "\""})
 	}
-	return urlList
+	return csvEntryList
 }
 
 // This will use SEMRush API for every url in urlList to get a domain score
-func processUrlList(urlList []string, apiKey *string, lang *string) {
-	for i := 0; i < len(urlList); i++ {
-		domain := getDomainFromUrl(urlList[i])
+func processUrlList(csvEntryList []csvEntry, apiKey *string, lang *string) {
+	for i := 0; i < len(csvEntryList); i++ {
+		domain := getDomainFromUrl(csvEntryList[i].url)
 		score, ok := scoreMap[domain];
 		if !ok {
 			score = GetDomainScore(domain, apiKey, lang)
 			scoreMap[domain] = score
 		}
-		fmt.Printf("%s,%d,%s\n", domain, score, urlList[i])
+		fmt.Printf("%s,\"%d\"\n", csvEntryList[i].csv, score)
 	}
 }
 
