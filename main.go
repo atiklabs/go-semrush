f807f88e0913ff0bff6d83a09126f5fd3cf09d5e
Atik <atiklabs@pm.me>
2016-09-03 16:02:21 +0300

    License updated.

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Go SEMRush Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE.txt file.
+
 package main
 
 import (
